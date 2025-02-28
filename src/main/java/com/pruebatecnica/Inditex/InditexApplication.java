88de9bc17daf21a2518ff30c2502f52f6edccaf9
lcalderon94 <69425682+lcalderon94@users.noreply.github.com>
2025-03-01 00:04:39 +0100

    Creacion del proyecto

---
+++ src/main/java/com/pruebatecnica/Inditex/InditexApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.Inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatecnica/Inditex/InditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.Inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
