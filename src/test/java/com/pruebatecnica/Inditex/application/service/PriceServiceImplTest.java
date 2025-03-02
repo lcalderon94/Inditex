42012447c9bbeb14f02630e94031432dce5430b7
lcalderon0294 <luiscalderon0294@gmail.com>
2025-03-02 15:15:30 +0100

    no message

--- src/test/java/com/pruebatecnica/Inditex/application/service/PriceServiceImplTest.java
+++ src/test/java/com/pruebatecnica/Inditex/application/service/PriceServiceImplTest.java
@@ -1,10 +1,11 @@
 package com.pruebatecnica.Inditex.application.service;
 
+import com.pruebatecnica.Inditex.application.dto.PriceRequest;
+import com.pruebatecnica.Inditex.application.dto.PriceResponse;
 import com.pruebatecnica.Inditex.domain.exception.PriceNotFoundException;
 import com.pruebatecnica.Inditex.domain.model.Price;
 import com.pruebatecnica.Inditex.domain.port.input.PriceService;
-import com.pruebatecnica.Inditex.domain.port.input.PriceService.PriceRequest;
-import com.pruebatecnica.Inditex.domain.port.input.PriceService.PriceResponse;
+
 import com.pruebatecnica.Inditex.domain.port.output.PriceRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -87,7 +88,7 @@ class PriceServiceImplTest {
                 .build();
                 
         // Creación de un PriceRequest para pruebas de creación/actualización
-        priceRequest = PriceService.PriceRequest.builder()
+        priceRequest = PriceRequest.builder()
                 .brandId(1L)
                 .startDate(LocalDateTime.parse("2023-01-01T00:00:00"))
                 .endDate(LocalDateTime.parse("2023-12-31T23:59:59"))
