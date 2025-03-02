7f274dbe7966543e07020dd8ef34992d42f809c6
lcalderon0294 <luiscalderon0294@gmail.com>
2025-03-02 03:57:03 +0100

    Primer test de integracion

---
+++ src/test/java/com/pruebatecnica/Inditex/infrastucture/adapter/input/rest/PriceControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.pruebatecnica.Inditex.infrastucture.adapter.input.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PriceControllerIntegrationTest {
+	
+	@Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test1_shouldReturnPriceAt10amOnDay14() throws Exception {
+        mockMvc.perform(get("/api/prices/applicable")
+                .param("date", "2020-06-14T10:00:00")
+                .param("productId", "35455")
+                .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId", is(35455)))
+                .andExpect(jsonPath("$.brandId", is(1)))
+                .andExpect(jsonPath("$.priceList", is(1)))
+                .andExpect(jsonPath("$.price", is(35.50)));
+    }
+
+}
